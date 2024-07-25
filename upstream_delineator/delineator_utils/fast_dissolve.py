bf7ac44a085bc651931b522bfd8a269ba825aedc
Dan Katz <danielkatz125@gmail.com>
2024-07-25 09:19:59 -0400

    Remove pygeos (#8)
    * Don't use pygeos
    
    * Don't use pygeos
    
    ---------
    
    Co-authored-by: Dan Katz <dan@upstream.tech>

--- upstream_delineator/delineator_utils/fast_dissolve.py
+++ upstream_delineator/delineator_utils/fast_dissolve.py
@@ -19,7 +19,6 @@ with my watershed boundaries.
 
 import geopandas as gpd
 from shapely.geometry import Polygon, MultiPolygon
-gpd.options.use_pygeos = True
 
 
 def buffer(poly: Polygon) -> Polygon:
